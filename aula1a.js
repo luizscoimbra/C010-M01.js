e94667489345c9d641f646a14ea58a434e3dc4d4
LUIZ ALBERTO COIMBRA <53239828+luizscoimbra@users.noreply.github.com>
2021-09-22 22:49:57 +0000

    Initial commit

---
+++ aula1a.js
@@ -0,0 +1,22 @@
+console.log('Olá Mundo!');
+
+let nomeCompleto = "Luiz Coimbra";
+let nomecompleto = "Luiz Coimbra 2";
+
+console.log(nomeCompleto);
+console.log(nomecompleto);
+
+let paisAmerica = prompt("Digite um país da América>=: ");
+let idade = +prompt("Digite sua idade: ");
+
+console.log(paisAmerica);
+console.log(idade);
+
+//Exibe o tipo da variavel selecioanda.
+typeof(paisAmerica);
+
+let testeBool = 5 == "5";
+
+typeof(testeBool);
+
+
