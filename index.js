2e4464c8d4993ea5a2939ef831364a253a170f56
LUIZ ALBERTO COIMBRA <53239828+luizscoimbra@users.noreply.github.com>
2021-09-29 22:18:16 +0000

    Atualização no código Jokenpo

--- index.js
+++ index.js
@@ -1,35 +1,80 @@
+console.log();
+console.log("#############################");
+console.log("#############################");
+console.log("####### J O K E N P O #######");
+console.log("#############################");
+console.log("#############################");
+console.log();
 
-resposta = prompt('Qual sua jogada, pedra, papel ou tesoura? ');
+rodadas = prompt("Escolha a quantidade de rodadas: ");
+console.log();
+n = 0;
+n = rodadas + 1;
+
+while(n = rodadas){
+
+resposta = prompt('Qual sua jogada, PEDRA, PAPEL ou TESOURA? ');
+console.log();
 
 const opcoes = {
   pedra: 1,
   papel: 2,
   tesoura: 3
 }
-const escolhaDoUsuario = opcoes[resposta];
-const escolhaDoComputador = Math.round(Math.random() * 2) + 1;
+const usuario = opcoes[resposta];
+const maquina = Math.round(Math.random() * 2) + 1;
 
-if (escolhaDoUsuario === 1 && escolhaDoComputador === 3) {
-    console.log('Você ganhou!');
-}else if (escolhaDoUsuario === 1 && escolhaDoComputador === 2) {
-    console.log('Você perdeu!');
-}else if (escolhaDoUsuario === 1 && escolhaDoComputador === 1) {
-    console.log('Empatou!');
+if (usuario === 1 && maquina === 3) {
+    console.log('Parabéns, você ganhou!');
+    console.log();
+}else if (usuario === 1 && maquina === 2) {
+    console.log('Que pena, escolha errada, você perdeu!');
+    console.log();
+}else if (usuario === 1 && maquina === 1) {
+    console.log('Parece que o computador advinhou sua ecolha, vocês empataram!');
+    console.log();
 }
 
-if (escolhaDoUsuario === 2 && escolhaDoComputador === 1) {
-    console.log('Você ganhou!');
-}else if (escolhaDoUsuario === 2 && escolhaDoComputador === 3) {
-    console.log('Você perdeu!');
-}else if (escolhaDoUsuario === 2 && escolhaDoComputador === 2) {
-    console.log('Empatou!');
+if (usuario === 2 && maquina === 1) {
+    console.log('Parabéns, você ganhou!');
+   
+}else if (usuario === 2 && maquina === 3) {
+    console.log('Que pena, escolha errada, você perdeu!');
+    
+}else if (usuario === 2 && maquina === 2) {
+    console.log('Parece que o computador advinhou sua ecolha, vocês empataram!');
+    
 }
 
-if (escolhaDoUsuario === 3 && escolhaDoComputador === 2) {
-    console.log('Você ganhou!');
-}else if (escolhaDoUsuario === 3 && escolhaDoComputador === 1) {
-    console.log('Você perdeu!');
-}else if (escolhaDoUsuario === 3 && escolhaDoComputador === 3) {
-    console.log('Empatou!');
+if (usuario === 3 && maquina === 2) {
+    console.log('Parabéns, você ganhou!');
+    
+}else if (usuario === 3 && maquina === 1) {
+    console.log('Você perdeu!Que pena, escolha errada, você perdeu!');
+   
+}else if (usuario === 3 && maquina === 3) {
+    console.log('Parece que o computador advinhou sua ecolha, vocês empataram!');
+    
 }
 
+console.log("Você escolheu "+resposta+"!!!");
+
+if (maquina === 1){
+  console.log("Maquina escolheu pedra!!!");
+  console.log();
+}
+if (maquina === 2){
+  console.log("Maquina escolheu papel!!!");
+  console.log();
+}
+if (maquina === 3){
+  console.log("Maquina escolheu tesoura!!!");
+  console.log();
+}
+}
+console.log();
+console.log("#################################");
+console.log("# ALUNO: LUIZ ALBERTO COIMBRA   #");
+console.log("# TURMA: C010 MOD:001           #");
+console.log("# MODULO: LÓGICA DE PROGRAMAÇÃO #");
+console.log("#################################");
