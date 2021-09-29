bc41b1e1267bc60ac817602595395abf0352cda0
LUIZ ALBERTO COIMBRA <53239828+luizscoimbra@users.noreply.github.com>
2021-09-29 00:05:49 +0000

    Inserido Jokenpo

---
+++ index.js
@@ -0,0 +1,35 @@
+
+resposta = prompt('Qual sua jogada, pedra, papel ou tesoura? ');
+
+const opcoes = {
+  pedra: 1,
+  papel: 2,
+  tesoura: 3
+}
+const escolhaDoUsuario = opcoes[resposta];
+const escolhaDoComputador = Math.round(Math.random() * 2) + 1;
+
+if (escolhaDoUsuario === 1 && escolhaDoComputador === 3) {
+    console.log('Você ganhou!');
+}else if (escolhaDoUsuario === 1 && escolhaDoComputador === 2) {
+    console.log('Você perdeu!');
+}else if (escolhaDoUsuario === 1 && escolhaDoComputador === 1) {
+    console.log('Empatou!');
+}
+
+if (escolhaDoUsuario === 2 && escolhaDoComputador === 1) {
+    console.log('Você ganhou!');
+}else if (escolhaDoUsuario === 2 && escolhaDoComputador === 3) {
+    console.log('Você perdeu!');
+}else if (escolhaDoUsuario === 2 && escolhaDoComputador === 2) {
+    console.log('Empatou!');
+}
+
+if (escolhaDoUsuario === 3 && escolhaDoComputador === 2) {
+    console.log('Você ganhou!');
+}else if (escolhaDoUsuario === 3 && escolhaDoComputador === 1) {
+    console.log('Você perdeu!');
+}else if (escolhaDoUsuario === 3 && escolhaDoComputador === 3) {
+    console.log('Empatou!');
+}
+
