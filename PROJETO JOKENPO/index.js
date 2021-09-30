208403ba4e1b1125cc93386cef6d58f96157e291
LUIZ ALBERTO COIMBRA <53239828+luizscoimbra@users.noreply.github.com>
2021-09-30 17:19:05 +0000

    Atualização codigo Jokenpo

---
+++ PROJETO JOKENPO/index.js
@@ -0,0 +1,9 @@
+let rodadas = prompt("Digite o numero de rodadas: ");
+
+for (let i = 0; i <= rodadas; i++)
+{
+  console.log(i);
+}
+
+let quantidade = rodadas.length;
+console.log(rodadas);
--- index.js
+++ index.js
@@ -6,58 +6,89 @@ console.log("#############################");
 console.log("#############################");
 console.log();
 
-rodadas = prompt("Escolha a quantidade de rodadas: ");
-console.log();
-n = 0;
-n = rodadas + 1;
+let rodadas = prompt("Digite o numero de rodadas: ");
 
-while(n = rodadas){
-
-resposta = prompt('Qual sua jogada, PEDRA, PAPEL ou TESOURA? ');
+console.log();  
+console.log("VOCE ESCOLHEU JOGAR "+rodadas+" VEZES!");
 console.log();
+
+//Laço de repetição que permite usuario escolher quantas rodadas vai querer jogar na disputa.
+
+for (let i = 1; i <= rodadas; i++)
+{resposta = prompt('Qual sua jogada, PEDRA, PAPEL ou TESOURA? ');
+console.log();
+
+//Variavel armazena resposta e referencia um número correspondente.
 
 const opcoes = {
   pedra: 1,
   papel: 2,
   tesoura: 3
 }
+
+//Função Math.random realiza sortei das opções de foram aleatória.
 const usuario = opcoes[resposta];
 const maquina = Math.round(Math.random() * 2) + 1;
+
+//Armazena quantidade vitorias da maquina e do usuario.
+
+contUse = 0;
+contMaq = 0;
+
+//Inicia lógica do jogo.
+
+//Primeira possibilidade.
 
 if (usuario === 1 && maquina === 3) {
     console.log('Parabéns, você ganhou!');
     console.log();
+    contUse = contUse + 1;
+
 }else if (usuario === 1 && maquina === 2) {
     console.log('Que pena, escolha errada, você perdeu!');
     console.log();
+    contMaq = contMaq + 1;
+
 }else if (usuario === 1 && maquina === 1) {
     console.log('Parece que o computador advinhou sua ecolha, vocês empataram!');
     console.log();
 }
 
+//Segunda possibilidade.
+
 if (usuario === 2 && maquina === 1) {
     console.log('Parabéns, você ganhou!');
-   
+    contUse = contUse + 1;
+
 }else if (usuario === 2 && maquina === 3) {
     console.log('Que pena, escolha errada, você perdeu!');
-    
+    contMaq = contMaq + 1;
+
 }else if (usuario === 2 && maquina === 2) {
     console.log('Parece que o computador advinhou sua ecolha, vocês empataram!');
     
 }
 
+//Terceira possibilidade.
+
 if (usuario === 3 && maquina === 2) {
     console.log('Parabéns, você ganhou!');
-    
+    contUse = contUse + 1; 
+
 }else if (usuario === 3 && maquina === 1) {
     console.log('Você perdeu!Que pena, escolha errada, você perdeu!');
-   
+    contMaq = contMaq + 1;
+
 }else if (usuario === 3 && maquina === 3) {
     console.log('Parece que o computador advinhou sua ecolha, vocês empataram!');
     
 }
 
 console.log("Você escolheu "+resposta+"!!!");
+console.log("Você venceu "+contUse+" vezes!");
+console.log("O computador venceu "+contMaq+" vezes!");
+
+//Apresenta as escolhas da maquina.
 
 if (maquina === 1){
   console.log("Maquina escolheu pedra!!!");
@@ -72,6 +103,7 @@ if (maquina === 3){
   console.log();
 }
 }
+
 console.log();
 console.log("#################################");
 console.log("# ALUNO: LUIZ ALBERTO COIMBRA   #");
